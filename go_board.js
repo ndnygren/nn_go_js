52f3016bce78b6a4e45209e14e55e796eda9c5cd
Nick Nygren <nnygren@shaw.ca>
2016-07-04 10:18:53 -0600

    layout

---
+++ go_board.js
@@ -0,0 +1,28 @@
+
+function GoBoard(size) {
+	this.size = size;
+	this.data = [];
+	for (var i = 0; i < size; i++) {
+		data.push([]);
+		for (var j = 0; j < size; j++) {
+			data[i].push(0);
+		}
+	}
+
+	this.get = function(i,j) {
+		if (i < 0 || i >= this.size) { throw(i + " is out of range."); }
+		if (j < 0 || j >= this.size) { throw(j + " is out of range."); }
+		return this.data[i][j];
+	}
+
+	this.copy = function() {
+		var output = new GoBoard(this.size);
+		for (var i = 0; i < this.size; i++) {
+			for (var j = 0; j < this.size; j++) {
+				output.data[i][j] = this.get(i,j);
+			}
+		}
+		return output;
+	}
+}
+
