daea09a4ed091edd38bbe5963bee5ea58a61d14c
Nick Nygren <nnygren@shaw.ca>
2016-07-12 18:23:37 -0600

    added licenses

--- go_canvas.js
+++ go_canvas.js
@@ -1,3 +1,19 @@
+/*
+* nn_go_js - WebUI for Go, deployed on nygren.ca
+* Copyright (C) 2016 Nick Nygren
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 
 function GameManagerInt(canvas, gamelist, swindow, cwindow, uid) {
 	this.cw = new CanvasWriter(new GoBoard(5), canvas);
