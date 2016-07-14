df968493a17f2fc1b39f1f95ae58a1eab2678001
Nick Nygren <nnygren@shaw.ca>
2016-07-14 11:18:12 -0600

    gnugo can end the game now

--- gnugo_connect.js
+++ gnugo_connect.js
@@ -6,8 +6,10 @@ var querystring = require('querystring');
 
 var host = "192.168.1.32";
 var url = "/nn_go_js/go_json.php";
-var user_id = 1;
-var session_id = 79660248;
+var user_id = 2;
+var session_id = 78521752;
+var go_board_data = fs.readFileSync('./go_board.js','utf8');
+eval(go_board_data);
 
 function breakOnDelim(input, delim) {
 	var output = [];
@@ -24,11 +26,13 @@ function breakOnDelim(input, delim) {
 }
 
 function locToAlphaNum(loc) {
-	return String.fromCharCode(65 + loc[0])+loc[1];
+	if (loc[0] == -1) { return "pass"; }
+	return String.fromCharCode(65 + loc[0])+(loc[1]+1);
 }
 
 function alphaNumToLoc(alpha) {
 	var first = alpha.toLowerCase().charCodeAt(0) - 97;
+	if (alpha == "PASS") { return {"l": -1, "r": -1}; }
 	return {"l": first, "r": parseInt(alpha.slice(1)) - 1};
 }
 
@@ -46,22 +50,43 @@ function adaptSeqToGTP(game_obj) {
 	return output;
 }
 
+function makePassReq(obj) {
+	var board = new GoBoard(obj.size).addSeq(obj.seq);
+	var ter_score = board.scoreFromMap();
+	var cap_score = board.captureCount();
+	var req = {"type": "pass", "id": obj.id, "b": ter_score.b+cap_score.b, "w":ter_score.w+cap_score.w};
+	return req;
+}
+
 function findAndSendMove(obj) {
-	fs.writeFile("movefile.txt", adaptSeqToGTP(obj), function(err) {
-		exec("gnugo --mode gtp < movefile.txt", function (error, stdout, stderr) {
+	var req = {"type": "move", "uid": user_id};
+	var loc;
+	fs.writeFile("movefile"+obj.id+".txt", adaptSeqToGTP(obj), function(err) {
+		exec("gnugo --mode gtp < movefile"+obj.id+".txt", function (error, stdout, stderr) {
 			var parts = breakOnDelim(stdout, "=");
 			parts = parts.map(function (x) {
 				return x.replace(/^[ \r\n]+/, "").replace(/[ \r\n]+$/, "");
 			}).filter(function (x) {
 				return x != "";
 			});
-			if (parts.length > 1) {
+			if (parts.length != 1) {
 				throw("An error occured: " + JSON.stringify(parts));
 			}
-
-			console.log(obj.id + ": " + JSON.stringify(alphaNumToLoc(parts[0])));
+			loc = alphaNumToLoc(parts[0]);
+			console.log(obj.id + ": " + JSON.stringify(loc));
+			req.id = obj.id;
+			req.l = loc.l;
+			req.r = loc.r;
+			if (req.l == -1) { req = makePassReq(obj); }
+			doPost(host, url, user_id, session_id, req, function (data) {});
 		});
 	});
+}
+
+function myTurn(obj, user_id) {
+	if (obj.buid == user_id && obj.seq.length % 2 == 0) { return true; }
+	else if (obj.wuid == user_id && obj.seq.length % 2 == 1) { return true; }
+	else { return false; }
 }
 
 function getGames(host, url, user_id, session_id) {
@@ -70,7 +95,9 @@ function getGames(host, url, user_id, session_id) {
 		var obj = JSON.parse(data);
 		var list = obj.detail;
 		for (var i = 0; i < list.length; i++) {
-			findAndSendMove(list[i]);
+			if (myTurn(list[i],user_id)) {
+				findAndSendMove(list[i]);
+			}
 		}
 	});
 }
@@ -99,5 +126,7 @@ function doPost(host, url, user_id, session_id, req, callback) {
 	req.end();
 }
 
-getGames(host, url, user_id, session_id);
+setInterval(function (x) {
+	getGames(host, url, user_id, session_id);
+}, 3000);
 
