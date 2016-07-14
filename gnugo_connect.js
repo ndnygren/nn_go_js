897960c9d0e9af8755b13c464c0620fe90aff5c4
Nick Nygren <nnygren@shaw.ca>
2016-07-13 22:04:53 -0600

    querying server for game list

--- gnugo_connect.js
+++ gnugo_connect.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var sys = require('sys')
 var exec = require('child_process').exec;
+var http = require('http');
+var querystring = require('querystring');
 
-var fixt = [{ "id": "4", "buid": "40", "wuid": "1", "bname": "R2D2", "wname": "Nick", "size": "9", "seq": [ [2, 2], [6, 2] ] },
-	{ "id": "10", "buid": "2", "wuid": "1", "bname": "Brick", "wname": "Nick", "size": "9", "seq": [ [2, 3], [5, 2], [3, 2], [6, 2], [1, 3], [2, 1], [3, 1] ]
-	},
-{ "id": "11", "buid": "41", "wuid": "1", "bname": "Clamp", "wname": "Nick", "size": "9", "seq": [] }];
+var host = "192.168.1.32";
+var url = "/nn_go_js/go_json.php";
+var user_id = 1;
+var session_id = 79660248;
 
 function breakOnDelim(input, delim) {
 	var output = [];
@@ -44,20 +46,58 @@ function adaptSeqToGTP(game_obj) {
 	return output;
 }
 
-fs.writeFile("movefile.txt", adaptSeqToGTP(fixt[1]), function(err) {
-	exec("gnugo --mode gtp < movefile.txt", function (error, stdout, stderr) {
-		var parts = breakOnDelim(stdout, "=");
-		parts = parts.map(function (x) {
-			return x.replace(/^[ \r\n]+/, "").replace(/[ \r\n]+$/, "");
-		}).filter(function (x) {
-			return x != "";
+function findAndSendMove(obj) {
+	fs.writeFile("movefile.txt", adaptSeqToGTP(obj), function(err) {
+		exec("gnugo --mode gtp < movefile.txt", function (error, stdout, stderr) {
+			var parts = breakOnDelim(stdout, "=");
+			parts = parts.map(function (x) {
+				return x.replace(/^[ \r\n]+/, "").replace(/[ \r\n]+$/, "");
+			}).filter(function (x) {
+				return x != "";
+			});
+			if (parts.length > 1) {
+				throw("An error occured: " + JSON.stringify(parts));
+			}
+
+			console.log(obj.id + ": " + JSON.stringify(alphaNumToLoc(parts[0])));
 		});
-		if (parts.length > 1) {
-			throw("An error occured: " + JSON.stringify(parts));
-		}
-
-		console.log(JSON.stringify(alphaNumToLoc(parts[0])));
 	});
-});
+}
 
+function getGames(host, url, user_id, session_id) {
+	var req = {"type": "games", "uid": user_id};
+	doPost(host, url, user_id, session_id, req, function (data) {
+		var obj = JSON.parse(data);
+		var list = obj.detail;
+		for (var i = 0; i < list.length; i++) {
+			findAndSendMove(list[i]);
+		}
+	});
+}
+
+function doPost(host, url, user_id, session_id, req, callback) {
+	var cookie = "u_t_index="+user_id+"; Session_Id=" + session_id;
+	var qs = querystring.stringify({'request': JSON.stringify(req)});
+	var options = {
+		hostname: host,
+		port: 80,
+		path: url,
+		method: 'POST',
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+			'Cookie': cookie,
+			'Content-Length': Buffer.byteLength(qs)
+		}
+	};
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', callback);
+	});
+	req.write(qs);
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+	req.end();
+}
+
+getGames(host, url, user_id, session_id);
 
